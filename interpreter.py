f79d0020e30c126d6ffd999592629c733d8eff7d
anna-ilina <25167864+anna-ilina@users.noreply.github.com>
2021-01-01 23:17:42 -0500

    Support numeric variables

--- interpreter.py
+++ interpreter.py
@@ -9,7 +9,6 @@ Interpreter steps:
     3) Evaluation
 
 Future work:
-    - support variables
     - support negative integer inputs
     - support decimal inputs
     - support parantheses
@@ -18,6 +17,10 @@ Future work:
 """
 
 OPERATORS = {
+    '=': {
+        'fn': None,  # special operator, handled within evaluate fn
+        'precedence': 0,
+    },
     '+': {
         'fn': operator.add,
         'precedence': 1,
@@ -36,7 +39,6 @@ OPERATORS = {
     },
 }
 
-
 class BaseError(Exception):
     def __init__(self, message):
         self.message = message
@@ -49,6 +51,12 @@ class InputError(BaseError):
 class ParsingError(BaseError):
     pass
 
+
+class InvalidAssignmentSyntaxError(BaseError):
+    pass
+
+class VariableNotDefinedError(BaseError):
+    pass
 
 # Convert input string into list of separate tokens
 # Token elements can be integers or operator chars
@@ -70,6 +78,15 @@ def tokenize(input_string):
             tokens.append(int(input_string[i:end_index]))
             i = end_index
 
+        # Handle variable name inputs
+        elif input_string[i] in string.ascii_letters:
+            end_index = i+1
+            valid_variable_chars = string.digits + string.ascii_letters
+            while end_index < len(input_string) and input_string[end_index] in valid_variable_chars:
+                end_index += 1
+            tokens.append(input_string[i:end_index])
+            i = end_index
+
         # Handle operator inputs
         elif input_string[i] in OPERATORS.keys():
             tokens.append(input_string[i])
@@ -88,10 +105,10 @@ def tokenize(input_string):
 # (Or if tokenized input contains only an int value and no operators, return the value)
 def parse_to_tree(tokenized_input):
     if len(tokenized_input) == 1:
-        if isinstance(tokenized_input[0], (int, float)):
-            return tokenized_input[0]
-        else:
+        if tokenized_input[0] in OPERATORS.keys():
             raise ParsingError('Invalid input expression')
+        else:
+            return tokenized_input[0]
 
     lowest_precedence = 3 # Max precedence
     lowest_precedence_op_index = None
@@ -113,24 +130,107 @@ def parse_to_tree(tokenized_input):
         raise ParsingError('Invalid input expression')
 
 
+"""
+# Sample structure of variables prefix tree
+{
+    'a': {
+        'p': {
+            'p': {
+                '\n': 1,
+                's': {
+                    '\n': 101
+                }
+            }
+        },
+        'z': {'\n': 23}
+    }
+}
+"""
+
+
+# Add variable to variables prefix tree, overwriting previous value
+# if it exists
+def assign_variable(variables_tree, name, value):
+    tree_pointer = variables_tree
+    for char in name:
+        # Assume variable values are always numeric (cannot be set to None)
+        if tree_pointer.get(char) is None:
+            tree_pointer[char] = {}
+
+        tree_pointer = tree_pointer[char]
+
+    # Use '\n' as sentinal to mark end of variable
+    tree_pointer['\n'] = value
+
+
+# Walk through variables prefix tree, and return value of variable given by name
+# If variable with given name does not exist, return None
+def get_variable(variables_tree, name):
+    tree_pointer = variables_tree
+    for char in name:
+        if tree_pointer.get(char) is None:
+            return None
+
+        tree_pointer = tree_pointer[char]
+
+    return tree_pointer.get('\n')
+
+
+# Valid variable names start with a letter
+# and contain only alphanumeric characters
+def valid_variable_name(name):
+    if not isinstance(name, str):
+        return False
+
+    if len(name) == 0 or name[0] not in string.ascii_letters:
+        return False
+
+    for char in name[1:]:
+        if char not in string.ascii_letters and char not in string.digits:
+            return False
+
+    return True
+
+
 # Evaluate expression encoded by input tree
-def evaluate(tree):
-    if isinstance(tree, int):
-        # Handle case where user enters a value string (e.g. '101'), not an expression
+def evaluate(tree, variables):
+    # Base case: value is numeric
+    if isinstance(tree, (int, float)):
         return tree
 
-    return OPERATORS[tree['op']]['fn'](
-        evaluate(tree['left']) if isinstance(tree['left'], dict) else tree['left'],
-        evaluate(tree['right']) if isinstance(tree['right'], dict) else tree['right'],
-    )
+    # Base case: value is variable name
+    elif isinstance(tree, str):
+        value = get_variable(variables, tree)
+        if value is not None:
+            return value
+        else:
+            raise VariableNotDefinedError('Variable not exists: ' + tree)
+
+    # Handle variable assignment
+    elif tree['op'] == '=':
+        if valid_variable_name(tree['left']):
+            value = evaluate(tree['right'], variables)
+            assign_variable(variables, tree['left'], value)
+            return value
+        else:
+            raise InvalidAssignmentSyntaxError(
+                'Left side of assignment expression must be alphanumeric string starting with a letter'
+            )
+
+    # Evaluate expression
+    else:
+        return OPERATORS[tree['op']]['fn'](
+            evaluate(tree['left'], variables),
+            evaluate(tree['right'], variables),
+        )
 
 
 def main():
-    # variables = {}
+    variables = {}
 
-    print("Enter 'q' to exit input loop")
+    print("This is a simple interpreter program\nEnter 'q' to exit input loop")
     while True:
-        input_string = input('Input here: ')
+        input_string = input('>> ')
 
         if input_string == 'q':
             print("Exiting")
@@ -148,10 +248,10 @@ def main():
             print("PARSED INPUT TREE", json.dumps(parsed_input_tree, indent=4))
 
             # Evaluate expression
-            result = evaluate(parsed_input_tree)
+            result = evaluate(parsed_input_tree, variables)
             print("RESULT", result)
 
-        except (InputError, ParsingError) as e:
+        except (InputError, ParsingError, VariableNotDefinedError, InvalidAssignmentSyntaxError) as e:
             print(e.message + ", please try again")
 
 
--- test_interpreter.py
+++ test_interpreter.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
-from interpreter import tokenize, parse_to_tree, evaluate
+from interpreter import (
+    tokenize, parse_to_tree, evaluate, assign_variable, get_variable, valid_variable_name,
+    InputError, ParsingError, VariableNotDefinedError
+)
 
 
 class Test_Interpreter(TestCase):
@@ -9,7 +12,7 @@ class Test_Interpreter(TestCase):
         self.assertEqual(tokenize('  \t\n '), [])
 
     def test_tokenize_nonempty(self):
-        self.assertEqual(tokenize('123 +   5 / 6'), [123, '+', 5, '/', 6])
+        self.assertEqual(tokenize('123 + orange +  5 / 6'), [123, '+', 'orange', '+', 5, '/', 6])
 
     def test_parse_to_tree(self):
         self.assertEqual(
@@ -33,22 +36,214 @@ class Test_Interpreter(TestCase):
             }
         )
 
-    def test_evaluate(self):
+    def test_evaluate_numeric_value(self):
         self.assertEqual(
-            evaluate({
-                "op": "+",
-                "left": {
-                    "op": "*",
-                    "left": 10,
-                    "right": 2
+            evaluate(5, {}),
+            5
+        )
+
+    def test_evaluate_variableName(self):
+        variables_tree = {'a': {'p': {'p': {'\n': 123}}}}
+        self.assertEqual(
+            evaluate('app', variables_tree),
+            123
+        )
+
+    def test_evaluate_variableNameNotDefined(self):
+        variables_tree = {'a': {'p': {'p': {'\n': 123}}}}
+        with self.assertRaises(VariableNotDefinedError):
+            evaluate('ap', variables_tree)
+
+    def test_evaluate_expression(self):
+        self.assertEqual(
+            evaluate(
+                {
+                    "op": "+",
+                    "left": {
+                        "op": "*",
+                        "left": 10,
+                        "right": 2
+                    },
+                    "right": 5
                 },
-                "right": 5
-            }),
+                {}
+            ),
             25
         )
 
-    def test_evaluate_no_operators(self):
+    def test_evaluate_float_result(self):
         self.assertEqual(
-            evaluate(5),
-            5
+            evaluate(
+                {
+                    "op": "+",
+                    "left": {
+                        "op": "/",
+                        "left": 9,
+                        "right": 2
+                    },
+                    "right": 3
+                },
+                {}
+            ),
+            7.5
         )
+
+    def test_evaluate_expressionWithVariableName(self):
+        variables_tree = {'a': {'p': {'p': {'\n': 123}}}}
+        self.assertEqual(
+            evaluate(
+                {
+                    "op": "+",
+                    "left": {
+                        "op": "/",
+                        "left": 'app',
+                        "right": 2
+                    },
+                    "right": 3
+                },
+                variables_tree
+            ),
+            64.5
+        )
+
+    def test_evaluate_expressionWithVariableNameNotDefined(self):
+        variables_tree = {'a': {'p': {'p': {'\n': 123}}}}
+        with self.assertRaises(VariableNotDefinedError):
+            evaluate(
+                {
+                    "op": "+",
+                    "left": {
+                        "op": "/",
+                        "left": 'abc',
+                        "right": 2
+                    },
+                    "right": 3
+                },
+                variables_tree
+            )
+
+    def test_evaluate_assignVariable(self):
+        variables_tree = {'a': {'p': {'p': {'\n': 123}}}}
+        self.assertEqual(
+            evaluate(
+                {
+                    "op": "=",
+                    "left": "abc",
+                    "right": {
+                        "op": "/",
+                        "left": "app",
+                        "right": 2,
+                    }
+                },
+                variables_tree
+            ),
+            61.5
+        )
+        self.assertEqual(
+            variables_tree, 
+            {'a': {
+                'p': {'p': {'\n': 123}},
+                'b': {'c': {'\n': 61.5}}
+            }}
+        )
+
+    def test_evaluate_reassignVariable(self):
+        variables_tree = {'a': {'p': {'p': {'\n': 123}}}}
+        self.assertEqual(
+            evaluate(
+                {
+                    "op": "=",
+                    "left": "app",
+                    "right": {
+                        "op": "+",
+                        "left": "app",
+                        "right": 1,
+                    }
+                },
+                variables_tree
+            ),
+            124
+        )
+        self.assertEqual(
+            variables_tree, 
+            {'a': {'p': {'p': {'\n': 124}}}}
+        )
+
+
+    def test_assign_variable(self):
+        variables_tree = {}
+
+        # Assign variable
+        assign_variable(variables_tree, 'app', 123)
+
+        self.assertEqual(
+            variables_tree,
+            {'a': {'p': {'p': {'\n': 123}}}}
+        )
+
+        # Overwrite existing variable
+        assign_variable(variables_tree, 'app', 100)
+
+        self.assertEqual(
+            variables_tree,
+            {'a': {'p': {'p': {'\n': 100}}}}
+        )
+
+        # Assign overlapping variable
+        assign_variable(variables_tree, 'apps', 5)
+
+        self.assertEqual(
+            variables_tree,
+            {'a': {'p': {'p': {'\n': 100, 's': {'\n': 5}}}}}
+        )
+
+        # Assign overlapping variable
+        assign_variable(variables_tree, 'ap', 23)
+
+        self.assertEqual(
+            variables_tree,
+            {'a': {'p': {'\n': 23, 'p': {'\n': 100, 's': {'\n': 5}}}}}
+        )
+
+        # Assign non-overlpping variable
+
+        assign_variable(variables_tree, 'bee', 8)
+
+        self.assertEqual(
+            variables_tree,
+            {
+                'a': {'p': {'\n': 23, 'p': {'\n': 100, 's': {'\n': 5}}}},
+                'b': {'e': {'e': {'\n': 8}}}
+            }
+        )
+
+    def test_get_variable(self):
+        variables_tree = {
+            'a': {'p': {'\n': 23, 'p': {'\n': 100, 's': {'\n': 5}}}},
+            'b': {'e': {'e': {'\n': 8}}}
+        }
+
+        self.assertEqual(get_variable(variables_tree, 'app'), 100)
+
+        self.assertEqual(get_variable(variables_tree, 'apps'), 5)
+
+        self.assertEqual(get_variable(variables_tree, 'ap'), 23)
+
+        self.assertEqual(get_variable(variables_tree, 'bee'), 8)
+
+        self.assertEqual(get_variable(variables_tree, 'a'), None)
+
+        self.assertEqual(get_variable(variables_tree, 'bees'), None)
+
+        self.assertEqual(get_variable(variables_tree, 'ps'), None)
+
+    def test_valid_variable_name(self):
+        self.assertTrue(valid_variable_name('aBc123z'))
+
+        self.assertFalse(valid_variable_name(''))
+
+        self.assertFalse(valid_variable_name('1abc'))
+
+        self.assertFalse(valid_variable_name('abc#'))
+
+        self.assertFalse(valid_variable_name('abc d'))
